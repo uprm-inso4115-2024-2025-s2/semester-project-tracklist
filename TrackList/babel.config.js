2b6578daf4050c1f2af150647a42c34962547813
KevinIbarra1 <kevin.ibarra@upr.edu>
2025-02-15 16:08:25 -0400

    fixed babel config

--- TrackList/babel.config.js
+++ TrackList/babel.config.js
@@ -1,7 +1,7 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-      presets: ["babel-preset-expo"],
-      plugins: [["module:react-native-dotenv"]],
-    };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['expo-router/babel'], // Add this line
   };
+};
