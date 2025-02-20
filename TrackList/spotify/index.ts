3bc617d701d1a018c39364cb8e7083005440db41
DavidCuevas3 <david.cuevas3@upr.edu>
2025-02-20 11:33:18 -0400

    Basic function and test playlist added to debug

--- TrackList/spotify/index.ts
+++ TrackList/spotify/index.ts
@@ -3,3 +3,30 @@
 // export { default as trackService } from "./trackService";
 // export { default as artistService } from "./artistService";
 // export { default as playlistService } from "./playlistService";
+
+const CLIENT_ID = process.env.EXPO_PUBLIC_CLIENT_ID;
+const CLIENT_SECRET = process.env.EXPO_PUBLIC_CLIENT_SECRET;
+const testPlaylistID = '37i9dQZF1DZ06evO427oOs';
+
+
+export async function fetchSpotifyPlaylist(playlistId: string = "37i9dQZF1DZ06evO427oOs", accessToken: string) {
+    try {
+        const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error fetching playlist: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Failed to fetch playlist:", error);
+        return null;
+    }
+}
