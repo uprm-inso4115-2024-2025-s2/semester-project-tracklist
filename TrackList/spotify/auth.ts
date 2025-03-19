ee7b762c9bcec2b947ba3a4b0bca361793a845da
Javier Rivera <javier.rivera97@upr.edu>
2025-03-19 16:32:30 -0400

    Changed auth to use axios, related album methods in a same file and filtering based on data

--- TrackList/spotify/auth.ts
+++ TrackList/spotify/auth.ts
@@ -1,16 +1,27 @@
+import axios from "axios";
+
 const CLIENT_ID = process.env.EXPO_PUBLIC_SPOTIFY_CLIENT_ID;
 const CLIENT_SECRET = process.env.EXPO_PUBLIC_SPOTIFY_CLIENT_SECRET;
 const TOKEN_URL = process.env.EXPO_PUBLIC_SPOTIFY_TOKEN_URL as string;
 
+if (!CLIENT_ID || !CLIENT_SECRET || !TOKEN_URL) {
+  throw new Error("Missing Spotify API credentials");
+}
+
 export const fetchToken = async () => {
   try {
-    const response = await fetch(TOKEN_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
-    });
-    const data = await response.json();
-    return data.access_token;
+    const response = await axios.post(
+      TOKEN_URL,
+      new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: CLIENT_ID as string,
+        client_secret: CLIENT_SECRET as string,
+      }).toString(), 
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      }
+    );
+    return response.data.access_token;
   } catch (error) {
     console.error("Error fetching token:", error);
     return null;
